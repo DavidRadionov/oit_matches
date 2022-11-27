9640321ca275443e7eedebd8b12fe9ec2f4620a4
Gleb <gleb.batyan@gmail.com>
2022-11-27 16:39:55 +0300

    "Allahu Akbar"

--- oit/apps/oit_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OitAppConfig(AppConfig):
-    name = 'oit_app'
--- oit/urls.py
+++ oit/urls.py
@@ -1,28 +1,8 @@
-"""oit URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf.urls.static import static
-from django.conf import settings
-
+from oit import views
 
 urlpatterns = [
-    path('', include('oit_app.urls')),
-    path('admin/', admin.site.urls),
+    path('main/', views.games, name='games'),
+    path('game_analysis/<str:game_code>', views.game_analysis, name="game_analysis"),
 ]
-
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
