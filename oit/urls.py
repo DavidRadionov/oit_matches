d9fd13016147c8bbabf10cc0cd15e966881efc74
DavidRadionov <radionov.dava@gmail.com>
2022-11-28 01:21:03 +0300

    finalOCHKA

--- oit/urls.py
+++ oit/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from oit import views
 
 urlpatterns = [
-
-    path('', views.index, name='index'),
     path('main/', views.games, name='games'),
     path('game_analysis/<str:game_code>', views.game_analysis, name="game_analysis"),
 ]
